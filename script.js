3574817ba6f96cba3826b7e445fd4b73c0e54fff
Sarangshrikhande11 <x22202226@student.ncirl.ie>
2023-12-03 11:48:25 +0000

    error resolved

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://my-env2.eba-ywmjxpre.us-east-1.elasticbeanstalk.com';
+const baseURL = 'http://localhost:8000';
 
 const fetchData = (url, token = '') => {
     let hdrr = {
@@ -44,4 +44,47 @@ function postData(url, data, token = '') {
             console.error('There was a problem with the fetch operation:', error);
             throw error;
         });
+}
+
+async function getCart(){
+    return JSON.parse(localStorage.getItem("cart"));
+}
+
+async function addToCart(prod){
+    let cartNow = await getCart();
+    if(cartNow != null && cartNow?.some(c=>c.id == prod.id)){
+        let index = cartNow.findIndex(c=>c.id == prod.id);
+        cartNow[index] = {...cartNow[index],"quantity":(cartNow[index]['quantity'])+1}
+    }
+    else
+    {
+        if(cartNow == null){
+            cartNow = [{quantity:1,...prod}]
+        }
+        else
+        cartNow = [{quantity:1,...prod},...cartNow]
+    }
+    console.log('bef',cartNow);
+    localStorage.setItem("cart", JSON.stringify(cartNow));
+}
+
+
+async function deleteFromCart(prod){
+    let cartNow = await getCart();
+    let newCart = cartNow.filter(c=>c.id != prod.id);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+}
+
+async function reduceQantity(prod){
+    let cartNow = await getCart();
+    if(cartNow != null && cartNow?.some(c=>c.id == prod.id)){
+        let index = cartNow.findIndex(c=>c.id == prod.id);
+        if((cartNow[index]['quantity']) > 1)
+        cartNow[index] = {...cartNow[index],"quantity":(cartNow[index]['quantity'])-1}
+    }
+    localStorage.setItem("cart", JSON.stringify(cartNow));
+}
+
+async function emptyCart(){
+    localStorage.setItem("cart", JSON.stringify([]));
 }
